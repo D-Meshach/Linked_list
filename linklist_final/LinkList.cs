1df343c84faee72b7637aad644b1d4c5ec823b6f
D-Meshach <meshachdaniel456@gmail.com>
2023-03-28 03:39:18 +0530

    Welcome message

---
+++ linklist_final/LinkList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linklist_final
+{
+    public class LinkList
+    {
+        public LinkList() 
+        {
+            LinkListProcess processing = new LinkListProcess();
+            
+            Console.WriteLine("Reverse--");
+            processing.AddReverse(70);
+            processing.AddReverse(30);
+            processing.AddReverse(56);
+            processing.Displayreverse();
+        }
+       
+    }
+    public class LinkListProcess
+    {
+
+        public Node head,start;
+
+        public LinkListProcess()
+        {
+            this.head = null;
+            
+        }
+        public void AddReverse(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+                this.start = node;
+
+            }
+            else
+            {
+               
+                node.next = head;
+                head = node;
+            }
+
+
+        }
+        
+       
+        public void Displayreverse()
+        {
+            Node tmp=head;
+            while (tmp != null)
+            {
+                Console.WriteLine("Values are=" + tmp.data);
+                tmp = tmp.next;
+            }
+        }
+
+
+
+    }
+}
